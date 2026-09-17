ad94c0c5a430d59b5c64171f5450d1c109b7e769
Manh1007 <leducmanh488@gmail.com>
2026-09-17 23:23:49 +0700

    Add files via upload

---
+++ src/game/GameConfig.ts
@@ -0,0 +1,25 @@
+import Phaser from "phaser";
+
+import { BootScene } from "../scenes/BootScene";
+import { PreloadScene } from "../scenes/PreloadScene";
+import { MainMenuScene } from "../scenes/MainMenuScene";
+
+export const gameConfig: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+
+    width: 1280,
+    height: 720,
+
+    backgroundColor: "#1a1a2e",
+
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+
+    scene: [
+        BootScene,
+        PreloadScene,
+        MainMenuScene,
+    ],
+};
